c7d9be18b2de960620c2dce19da1ee1c9101560b
Vorobeva <natic_48@mail.ru>
2022-12-24 12:23:48 +0300

    Props+State

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
-import TextContent from './textContent';
+// import logo from './logo.svg';
+// import TextContent from './textContent';
 import './App.css';
+import { CounterComponent } from './CounterComponent';
+import { Input } from './Input';
+import assetsImage from './Assets/img.jpeg';
 
 function App() {
+  const handler = () => {
+    console.log('Кнопка была нажата');
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <TextContent />
-      </header>
+      <CounterComponent title={'Orange'} count={9} />
+      <CounterComponent title={'Apple'} count={4} />
+      <CounterComponent title={'Lemon'} count={3} />
+
+      <button onClick={handler}>PUSH ME</button>
+      <Input />
+      <img src={assetsImage} alt="img" />
+      <img src={'/img.jpeg'} alt="img" />
+      <div className="example">Hello World </div>
     </div>
   );
 }
